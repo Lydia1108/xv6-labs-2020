6360c5e21359d4d9e2acfb29fb8e6f0eae72d36b
Lydia1108 <134024939+Lydia1108@users.noreply.github.com>
2023-07-21 18:56:04 +0800

    Add files via upload

---
+++ user/xargs.c
@@ -0,0 +1,55 @@
+#include "kernel/types.h"
+#include "kernel/param.h"
+#include "user/user.h"
+#define max_length 512
+
+int main(int argc, char *argv[]) {
+    // 参数个数太大或太小
+    if (argc - 1 >= MAXARG || argc < 2) {
+        printf("error! xargs: please check the number of arguments.\n");
+        exit(1);
+    }
+    // 定义存储行的数组line、指向line的指针、存储参数的数组x_argv
+    char line[max_length];
+    char *p = line;
+    char *x_argv[MAXARG];
+    int i;
+    // 存储初始参数
+    for (i = 1; i < argc; i++) {
+        x_argv[i - 1] = argv[i];
+    }
+    //记录读取到数据的字节数
+    int rsz = sizeof(char);
+    while (rsz == sizeof(char)) {
+        int word_begin = 0;
+        int word_end = 0;
+        int arg_cnt = i - 1;
+        //读取一行
+        while (2) {
+            rsz = read(0, p, sizeof(char));
+            if (++word_end >= max_length) {
+                printf("xargs: arguments too long.\n");
+                exit(1);
+            }
+            if (*p == ' ' || *p == '\n' || rsz != sizeof(char)) {
+                //用字符串结束标志替换' '，'\n'和空字符
+                *p = 0;
+                x_argv[arg_cnt++] = &line[word_begin];
+                //表明下一个参数的位置，在当前参数结束符之后
+                word_begin = word_end;
+                if (arg_cnt >= MAXARG) {
+                    printf("xargs: too many arguments.\n");
+                    exit(1);
+                }
+                if (*p == '\n' || rsz != sizeof(char))
+                    break;
+            }
+            ++p;
+        }
+        if (fork() == 0) {
+            exec(argv[1], x_argv);
+        }
+        wait(0);
+    }
+    exit(0);
+}
